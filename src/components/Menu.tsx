fc250003c1b4b4224a99054227dcb1fe2a8ad8df
enesdmc <enesdmcc@gmail.com>
2023-11-13 21:18:34 +0300

    components menu completed

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,14 +1,34 @@
-import clsx from 'clsx'
-import React from 'react'
+import clsx from "clsx";
+import React from "react";
+import Icons, { IconNames } from "../utilities/icons";
 
 interface Props {
-  className: string
+  className: string;
 }
 
-const Menu:React.FC<Props> = ({className}) => {
+interface Data {
+  [key: string]: IconNames;
+}
+
+const Menu: React.FC<Props> = ({ className }) => {
+  const data: Data = {
+    "Menu Logo": "MenuLogo",
+    "weather": "MenuWeather",
+    "explore": "MenuExplore",
+    "cities": "MenuCities",
+    "settings": "MenuSettings",
+  };
+
   return (
-    <div className={clsx("", className)} >Menu</div>
-  )
-}
+    <div className={clsx("flex flex-col items-center justify-center gap-4", className)}>
+      {Object.entries(data).map(([key, value], index) => (
+        <div key={index} className="flex flex-col items-center justify-center">
+          <img src={Icons[value]} alt="Menu Logo" />
+          <span className={clsx("", key === "weather" ? "text-white" : "text-[#C4C3C1]" )}>{key}</span>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default Menu
+export default Menu;
---
+++ src/utilities/icons.tsx
@@ -0,0 +1,24 @@
+import MenuCities from "../assets/svg/menu/MenuCities.svg"
+import MenuSettings from "../assets/svg/menu/MenuSettings.svg"
+import MenuExplore from "../assets/svg/menu/MenuExplore.svg"
+import MenuLogo from "../assets/svg/menu/MenuLogo.svg"
+import MenuWeather from "../assets/svg/menu/MenuWeather.svg"
+
+const Icons = {
+    MenuCities,
+    MenuSettings,
+    MenuExplore,
+    MenuLogo,
+    MenuWeather,
+}
+
+export type IconNames =
+    | "MenuCities"
+    | "MenuSettings"
+    | "MenuExplore"
+    | "MenuLogo"
+    | "MenuWeather"
+
+
+
+export default Icons
